66703bb8a6ea4ce53deba641f08575518f87f19c
pedro <pedrodinizz2@outlook.com>
2023-08-18 18:10:11 -0300

    initial configuration

---
+++ src/test/java/io/github/flea777/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.flea777.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
