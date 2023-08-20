fb9ad45f7133b355f259610bb83fde0773b07fd2
pedro <pedrodinizz2@outlook.com>
2023-08-20 03:01:45 -0300

    postgresql configuration on properties, and entity created

---
+++ src/main/java/io/github/flea777/products/models/ProductModel.java
@@ -0,0 +1,24 @@
+package io.github.flea777.products.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_products")
+@Getter
+@Setter
+public class ProductModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduct;
+    private String name;
+    private BigDecimal value;
+}
